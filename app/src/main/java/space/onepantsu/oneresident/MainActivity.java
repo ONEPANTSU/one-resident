6b649f9b9f02fc27101885ede124e2c6b6ad9d32
ONEPANTSU <conandet@mail.ru>
2022-05-15 18:21:50 +0300

    Auto stash before rebase of "origin/Front"

--- app/src/main/java/space/onepantsu/oneresident/MainActivity.java
+++ app/src/main/java/space/onepantsu/oneresident/MainActivity.java
@@ -2,7 +2,10 @@ package space.onepantsu.oneresident;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button residentBttn = (Button) findViewById(R.id.residentButton);
+        Button historyBttn = (Button) findViewById(R.id.residentButton);
+        Button settingsBttn = (Button) findViewById(R.id.residentButton);
     }
+
+    public void onResidentActivity(View view){
+        Intent intent = new Intent(this, ResidentActivity.class);
+        startActivity(intent);
+    }
+
+    public void onHistoryActivity(View view){
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSettingsActivity(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/space/onepantsu/oneresident/ResidentActivity.java
@@ -0,0 +1,14 @@
+package space.onepantsu.oneresident;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResidentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resident);
+    }
+}
