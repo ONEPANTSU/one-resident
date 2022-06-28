9bb03d82bf29504b871bb6a3b88ad724508b98f2
ONEPANTSU <conandet@mail.ru>
2022-06-28 23:39:12 +0300

    fix: output from history_activity

--- app/src/main/java/space/onepantsu/oneresident/history/HistoryActivity.java
+++ app/src/main/java/space/onepantsu/oneresident/history/HistoryActivity.java
@@ -18,6 +18,9 @@ import space.onepantsu.oneresident.R;
 import space.onepantsu.oneresident.history.database.HistoryDB;
 import space.onepantsu.oneresident.history.database.HistoryDBMS;
 import space.onepantsu.oneresident.history.database.HistoryType;
+import space.onepantsu.oneresident.residents.ResidentActivity;
+import space.onepantsu.oneresident.residents.database.DBMS;
+import space.onepantsu.oneresident.residents.database.DataBase;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -86,7 +89,7 @@ public class HistoryActivity extends AppCompatActivity {
 
         TextView paymentText = view.findViewById(R.id.historyInfo);
 
-        String paymentTextBuilder = getHistoryInfo(historyInfo.currentID);
+        String paymentTextBuilder = getHistoryInfo(historyInfo);
 
         paymentText.setText(paymentTextBuilder);
 
@@ -94,8 +97,74 @@ public class HistoryActivity extends AppCompatActivity {
         linear.addView(view);
     }
 
-    private String getHistoryInfo(int id){
-        return null;
+    private String getHistoryInfo(HistoryInfo historyInfo){
+
+        String type = "";
+
+        ResidentActivity.ResidentInfo residentInfo = getResidentInfoByID(historyInfo.currentResidentID);
+
+        switch (historyInfo.currentType.toString()){
+            case "INCREASED_DEBT": type = "Арендатору необходимо внести оплату"; break;
+            case "WAS_PAID": type = "Арендатор внес оплата"; break;
+            case "CHANGED_DATA": type = "Перенос даты оплаты арендатора на " + residentInfo.currentDate; break;
+            case "DELETED_RESIDENT": type = "Арендатор был удалён"; break;
+            case "ADDED_RESIDENT": type = "Арендатор был дабвлен"; break;
+            default: break;
+        }
+
+        return historyInfo.currentDate + "\t" + residentInfo.currentSurname + " "
+                + residentInfo.currentName + "\n          \t" + type;
+    }
+
+    private ResidentActivity.ResidentInfo getResidentInfoByID(int id){
+        DBMS dbms = new DBMS(this);
+        SQLiteDatabase db = dbms.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + DataBase.ResidentsTable.TABLE_NAME +
+                " WHERE " + DataBase.ResidentsTable._ID + " = " + id;
+        @SuppressLint("Recycle") Cursor cursor = db.rawQuery(selectQuery, null);
+
+        int idColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable._ID);
+        int cityColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_CITY);
+        int streetColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_STREET);
+        int houseColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_HOUSE);
+        int levelColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_LEVEL);
+        int flatColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_FLAT);
+        int surnameColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_SURNAME);
+        int nameColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_NAME);
+        int secondnameColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_SECONDNAME);
+        int phoneColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_PHONE);
+        int dateColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_DATE);
+        int periodColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_PERIOD);
+        int priceColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_PRICE);
+        int commentColumnIndex = cursor.getColumnIndex(DataBase.ResidentsTable.COLUMN_COMMENT);
+
+        ResidentActivity.ResidentInfo residentInfo = new ResidentActivity.ResidentInfo();
+
+        while (cursor.moveToNext()) {
+            try {
+                residentInfo.currentID = cursor.getInt(idColumnIndex);
+                residentInfo.currentCity = cursor.getString(cityColumnIndex);
+                residentInfo.currentStreet = cursor.getString(streetColumnIndex);
+                residentInfo.currentHouse = cursor.getString(houseColumnIndex);
+                residentInfo.currentLevel = cursor.getInt(levelColumnIndex);
+                residentInfo.currentFlat = cursor.getInt(flatColumnIndex);
+                residentInfo.currentSurname = cursor.getString(surnameColumnIndex);
+                residentInfo.currentName = cursor.getString(nameColumnIndex);
+                residentInfo.currentSecondname = cursor.getString(secondnameColumnIndex);
+                residentInfo.currentPhone = cursor.getString(phoneColumnIndex);
+                residentInfo.currentDate = cursor.getString(dateColumnIndex);
+                residentInfo.currentPeriod = cursor.getInt(periodColumnIndex);
+                residentInfo.currentPrice = cursor.getInt(priceColumnIndex);
+                residentInfo.currentComment = cursor.getString(commentColumnIndex);
+
+            }
+            catch (Exception e){
+                System.out.println("Ошибка при чтении строки");
+            }
+        }
+
+        return residentInfo;
     }
 
     public void goBack(View view){
