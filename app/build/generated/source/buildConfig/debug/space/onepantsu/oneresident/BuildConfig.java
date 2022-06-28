b70f75a5ea5f44e2aa79422f608d75a355c3630b
ONEPANTSU <conandet@mail.ru>
2022-06-28 13:56:51 +0300

    Auto stash before rebase of "Notifications"

---
+++ app/build/generated/source/buildConfig/debug/space/onepantsu/oneresident/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package space.onepantsu.oneresident;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "space.onepantsu.oneresident";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
