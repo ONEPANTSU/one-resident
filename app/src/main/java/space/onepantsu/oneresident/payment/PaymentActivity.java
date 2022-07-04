ab1cf9b78c18f9cf20f69548ab15d76c4e288e57
nikramiar <nikramiar@gmail.com>
2022-07-04 16:04:30 +0300

    fix:newDate

--- app/src/main/java/space/onepantsu/oneresident/payment/PaymentActivity.java
+++ app/src/main/java/space/onepantsu/oneresident/payment/PaymentActivity.java
@@ -19,6 +19,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
